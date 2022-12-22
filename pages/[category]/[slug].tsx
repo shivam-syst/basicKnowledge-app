75ba53a815b6a80ededf8fad4c62a7ec2ea2a325
Shivam <Shivam74837@gmail.com>
2022-12-23 00:00:47 +0530

    dynamic nested folder not working

---
+++ pages/[category]/[slug].tsx
@@ -0,0 +1,94 @@
+import { useRouter } from "next/router";
+import ErrorPage from "next/error";
+import Container from "../../components/container";
+import PostBody from "../../components/post-body";
+import Header from "../../components/header";
+import PostHeader from "../../components/post-header";
+import Layout from "../../components/layout";
+import { getPostBySlug, getAllPosts } from "../../lib/api";
+import PostTitle from "../../components/post-title";
+import Head from "next/head";
+import markdownToHtml from "../../lib/markdownToHtml";
+import type PostType from "../../interfaces/post";
+
+type Props = {
+  post: PostType;
+  morePosts: PostType[];
+  preview?: boolean;
+};
+
+export default function Post({ post, morePosts, preview }: Props) {
+  const router = useRouter();
+  if (!router.isFallback && !post?.slug) {
+    return <ErrorPage statusCode={404} />;
+  }
+  return (
+    <Layout preview={preview}>
+      <Container>
+        <Header />
+        {router.isFallback ? (
+          <PostTitle>Loading…</PostTitle>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>{post.title}</title>
+                <meta property="og:image" content={post.ogImage.url} />
+              </Head>
+              <PostHeader
+                title={post.title}
+                coverImage={post.coverImage}
+                date={post.date}
+                author={post.author}
+              />
+              <PostBody content={post.content} />
+            </article>
+          </>
+        )}
+      </Container>
+    </Layout>
+  );
+}
+
+type Params = {
+  params: {
+    category?: string;
+    slug: string;
+  };
+};
+
+export async function getStaticPaths() {
+  const posts = getAllPosts(["slug", "category"], "_education");
+  console.log("posts", posts);
+  return {
+    paths: posts.map((post) => {
+      return {
+        params: {
+          category: `${post.category}`,
+          slug: post.slug,
+        },
+      };
+    }),
+    fallback: false,
+  };
+}
+
+export async function getStaticProps({ params }: Params) {
+  const directory = params.category;
+  console.log(`Params:`, params);
+  const post = getPostBySlug(
+    params.slug,
+    ["title", "date", "slug", "author", "content", "ogImage", "coverImage"],
+    `_${directory}`
+  );
+  const content = await markdownToHtml(post.content || "");
+
+  return {
+    props: {
+      post: {
+        ...post,
+        content,
+      },
+    },
+  };
+}
---
+++ pages/[category]/index.tsx
@@ -0,0 +1,52 @@
+import Container from "../../components/container";
+import AllStories from "../../components/all-stories";
+import Layout from "../../components/layout";
+import { getAllPosts } from "../../lib/api";
+import Head from "next/head";
+import Post from "../../interfaces/post";
+
+type Props = {
+  allPosts: Post[];
+  directory: string;
+};
+
+export default function Index({ allPosts, directory: dir }: Props) {
+  return (
+    <>
+      <Layout>
+        <Head>
+          <title> All posts</title>
+        </Head>
+        <Container>
+          {allPosts.length > 0 && <AllStories posts={allPosts} dir={dir} />}
+        </Container>
+      </Layout>
+    </>
+  );
+}
+
+export const getStaticPaths = async () => {
+  const CATEGORIES = ["sports", "education", "politics", "gadget", "posts"];
+  return {
+    paths: CATEGORIES.map((category) => {
+      return {
+        params: {
+          category,
+        },
+      };
+    }),
+    fallback: false,
+  };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const directory = params.category;
+  const allPosts = getAllPosts(
+    ["title", "date", "slug", "author", "coverImage", "excerpt"],
+    `_${directory}`
+  );
+
+  return {
+    props: { allPosts, directory },
+  };
+};
